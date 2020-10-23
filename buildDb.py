ee42870b47822687891448a3e3ad4c1d165c8e08
wallstalk <steppsip@gmail.com>
2020-10-23 12:10:50 +0300

    update

--- buildDb.py
+++ buildDb.py
@@ -1,9 +1,52 @@
 import sqlite3
 
 conn = sqlite3.connect('songs.db')
-
-
 c = conn.cursor()
-c.execute('''CREATE TABLE artists_X (id INTEGER PRIMARY KEY AUTOINCREMENT, author VARCHAR, song VARCHAR, lyrics TEXT)''')
-conn.commit()
+answer = c.execute('''SELECT * FROM artists_X;''')
+print(answer.fetchall())
 conn.close()
+
+songs = open(r"C:\Users\Cornucopia\PycharmProjects\LyricsToneSci\Lyrics\X.txt", "r", encoding="utf8").read()
+song_list = songs.split("#####\n")
+song_list.pop(0)
+song_title_list = []
+print(len(song_list))
+for song in song_list:
+    song_title_list.append(song.split('\n'))
+  # ([(song.partition('\n')[0]).partition('\n')[0], song.partition('\n')[2]])
+
+song_list_main = []
+first_row_val = 0
+for title in song_title_list:
+    song_list_main.append([title[2], title[0], '\n'.join(title[3:])])
+
+
+'''counter2 = 0
+for item in song_list_main:
+    ##if item[0][0] != 'X':
+    if item[2].count('\n') == 6:
+        print(item)
+        counter2 += 1
+
+print(counter2)'''
+print(len(song_list_main))
+artist_list = list(set([item[0] for item in song_list_main]))
+#for item in artist_list:
+
+'''for title in song_title_list:
+    if title[0] != "":
+        if title[2] != "":
+            print(title[0], title[2])
+    else:
+        if title[1] != "":
+            if title
+            print(title[1], title[2])
+        else:
+            if title[2] != "":
+                print(title[2])
+            else:
+                if title[3] != "":
+                    print(title[3])
+'''
+
+
--- spotipyAPI.py
+++ spotipyAPI.py
@@ -5,14 +5,55 @@ from spotipy.oauth2 import SpotifyClientCredentials
 auth_manager = SpotifyClientCredentials("1e42620b2b034f63bd21a1ff5b8526c1", "c587be0ae31d44f387f80d40aec3d3a7")
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-songs = open(r"C:\Users\Cornucopia\PycharmProjects\LyricsToneSci\Lyrics\X.txt", "r", encoding="utf8").read()
-#info_file = open("Documents/random/randoms/WebScraping/real/X_info.txt", "a")
-song_list = songs.split("#####")
-song_list.pop(0)
+'''
 title_list = [list(filter((str("")).__ne__, song.split("\n")))[:2] for song in song_list]
 big_list = list(set([item[1] for item in title_list]))
-big_list2 = []
+big_list2 = []'''
 
+songs = open(r"C:\Users\Cornucopia\PycharmProjects\LyricsToneSci\Lyrics\X.txt", "r", encoding="utf8").read()
+song_list = songs.split("#####\n")
+song_list.pop(0)
+song_title_list = []
+print(len(song_list))
+for song in song_list:
+    song_title_list.append(song.split('\n'))
+
+song_list_main = []
+first_row_val = 0
+for title in song_title_list:
+    song_list_main.append([title[2], title[0], '\n'.join(title[3:])])
+
+print(len(song_list_main))
+artist_list = list(set([item[0] for item in song_list_main]))
+print(len(artist_list))
+
+artist_dict = {}
+minilist = {}
+for name in artist_list:
+    results = sp.search(q='artist:' + name, type='artist', limit=50)
+    artists = results['artists']['items']
+    for item in artists:
+        artist = re.sub('[^A-Za-z0-9 ]+', '', item['name'])
+        if artist.upper() == name.upper():
+            artist_dict[name] = [item['id'], len(artists)]
+            break
+    minilist[name] = len(artists)
+
+print(len(artist_dict))
+print([[item, minilist[item]] for item in artist_list if item not in artist_dict.keys()])
+
+
+'''counter2 = 0
+for item in song_list_main:
+    ##if item[0][0] != 'X':
+    if item[2].count('\n') == 6:
+        print(item)
+        counter2 += 1
+
+print(counter2)'''
+'''print(len(song_list_main))
+artist_list = list(set([item[0] for item in song_list_main]))
+#for item in artist_list:
 for title in big_list:
     results = sp.search(q='artist:' + title, type='artist', limit=50)
     artists = results['artists']['items']
@@ -24,4 +65,4 @@ print(len(big_list), len(big_list2))
 print(big_list)
 print(big_list2)
 notfound = [item for item in big_list if item not in big_list2]
-print(len(notfound), notfound)
+print(len(notfound), notfound)'''
